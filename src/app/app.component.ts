bae870f8c6780d5d3936afd2e25ac6f4a972c197
SJH-706 <sjh-706@SJH-706-09.local>
2025-02-17 11:40:49 +0800

    Updated Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Template-Driven';
+  title = 'template-form-app';
+  anytext: string = "";
+  firstname: string = "";
+  lastname: string = "";
+  email: string = "";
+  contact: string = "";
+  submitted: boolean = false;
+
+onSubmit() {
+  this.submitted = true;
+}
+
 }
